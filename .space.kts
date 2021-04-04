35cd50525bed75e0d113c49ec63cd96032770be4
Pascal Krason <p.krason@padr.io>
2021-04-04 01:01:31 +0000

    Fixed typo in command

--- .space.kts
+++ .space.kts
@@ -6,7 +6,7 @@
 
 job("Build & Run") {
     container(displayName = "Composer", image = "composer:2") {
-     	args("compsoer", "install", "--no-dev", "--ignore-platform-reqs", "--no-interaction")
+     	args("composer", "install", "--no-dev", "--ignore-platform-reqs", "--no-interaction")
     }
     
     container(displayName = "PHP 7.4", image = "php:7.4") {
